10fd54e21e49d73905d46de891d68c0c5e22a807
RazielHernandez <razo.puma.azteca@gmail.com>
2023-01-21 23:04:24 -0500

    Third commit. Background added, some style to buttons and text size increased

--- MAPD724-W2023-SlotMachine/ContentView.swift
+++ MAPD724-W2023-SlotMachine/ContentView.swift
@@ -9,40 +9,67 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        ZStack {
+            Image("SlotMachineBackground")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
             
-            HStack{
-                Button("Quit Game"){
-                    print("Quit Game")
+            VStack{
+                HStack{
+                    Button("Quit Game"){
+                        print("Quit Game")
+                    }
+                    Spacer()
+                    Button("Reset Game"){
+                        print("Reset Game")
+                    }
                 }
+                .buttonStyle(.borderedProminent)
+                .tint(.orange)
+                .font(.system(size: 20))
+                
+                
+                Text("$1,000,000")
+                    .font(.system(size: 30))
+                Text("Jackpot")
+                    .font(.system(size: 30))
+                
                 Spacer()
-                Button("Reset Game"){
-                    print("Reset Game")
+                Rectangle()
+                Spacer()
+                
+                Button(action: { }) {
+                    Text("Spin")
+                        .padding()
+                        .bold()
+                        .font(.system(size: 30))
                 }
-            }
-            
-            Text("$1,000,000")
-            Text("Jackpot")
-            
-            Rectangle()
-            
-            Button("Spin"){
-                print("Spin")
-            }
-            
-            Text("Actual money: $ 4,500")
-            
-            HStack{
-                Button("-"){
-                    print("Reduce bet")
+                .foregroundColor(.white)
+                .background(.pink)
+                .cornerRadius(30)
+                
+                Text("Actual money: $ 4,500")
+                    .font(.system(size: 28))
+                
+                HStack{
+                    Button("-"){
+                        print("Reduce bet")
+                    }
+                    
+                    Text("BET: $ 500")
+                        .padding(.horizontal, 20)
+                    Button("+"){
+                        print("Increase bet")
+                    }
                 }
-                Text("BET: $ 500")
-                Button("+"){
-                    print("Increase bet")
-                }
+                .buttonStyle(.borderedProminent)
+                .font(.system(size: 30))
             }
+            .padding()
         }
-        .padding()
+            
+        
     }
 }
 
