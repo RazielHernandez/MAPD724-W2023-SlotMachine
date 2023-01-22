95e853d64618e4310ab02ac58c6405c0ebbf40c3
RazielHernandez <razo.puma.azteca@gmail.com>
2023-01-21 22:12:12 -0500

    Initial Commit

---
+++ MAPD724-W2023-SlotMachine/MAPD724_W2023_SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MAPD724_W2023_SlotMachineApp.swift
+//  MAPD724-W2023-SlotMachine
+//
+//  Created by Raziel Hernandez on 2023-01-21.
+//
+
+import SwiftUI
+
+@main
+struct MAPD724_W2023_SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
