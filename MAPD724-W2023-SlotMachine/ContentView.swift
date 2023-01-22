fd169c3ec2d19f3f01dfa2f1c062858e97c94dcc
RazielHernandez <razo.puma.azteca@gmail.com>
2023-01-21 22:24:04 -0500

    Second commit. Simple layout added, no logic or style

--- MAPD724-W2023-SlotMachine/ContentView.swift
+++ MAPD724-W2023-SlotMachine/ContentView.swift
@@ -10,10 +10,37 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            HStack{
+                Button("Quit Game"){
+                    print("Quit Game")
+                }
+                Spacer()
+                Button("Reset Game"){
+                    print("Reset Game")
+                }
+            }
+            
+            Text("$1,000,000")
+            Text("Jackpot")
+            
+            Rectangle()
+            
+            Button("Spin"){
+                print("Spin")
+            }
+            
+            Text("Actual money: $ 4,500")
+            
+            HStack{
+                Button("-"){
+                    print("Reduce bet")
+                }
+                Text("BET: $ 500")
+                Button("+"){
+                    print("Increase bet")
+                }
+            }
         }
         .padding()
     }
